eaf930168764b5377dbc3199e5f9792a9aece859
Samia <skouki@hct.ac.ae>
2023-01-29 12:03:06 +0400

    Initial Commit

---
+++ Jokes App/Jokes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_AppApp.swift
+//  Jokes App
+//
+//  Created by Apple on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
